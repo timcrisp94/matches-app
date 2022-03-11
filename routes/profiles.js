b021dc715cc21c8a5aae8998a740b141385b4f5b
Tim Crisp <timothycrisp@gmail.com>
2022-03-10 19:46:34 -0600

    heroku push

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,14 +2,12 @@ import { Router } from 'express'
 import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from "../controllers/profiles.js"
 
-
 const router = Router()
 
 // GET - localhost:3000/profiles
 router.get("/", isLoggedIn, profilesCtrl.index)
 // GET - localhost:3000/profiles/:id
 router.get('/:id', isLoggedIn, profilesCtrl.show)
-
 
 export {
   router
